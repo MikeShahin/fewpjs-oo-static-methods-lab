9390fbf63f8b52e5d3b0cc0afebab59eb3fcb91e
MikeShahin <gobias101@gmail.com>
2021-05-05 14:57:10 -0700

    done

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, "")
+  }
+
+  static titleize(title) {
+    let noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let solution = [];
+    let titleSplit = title.split(" ");
+
+    for (let i = 0; i < titleSplit.length; i++) {
+      if (i == 0) {
+        solution.push(this.capitalize(titleSplit[i]))
+      } else {
+        if (noCap.includes(titleSplit[i])) {
+          solution.push(titleSplit[i])
+        } else {
+          solution.push(this.capitalize(titleSplit[i]))
+        }
+      }
+    }
+    return solution.join( " " );
+  }
 }
